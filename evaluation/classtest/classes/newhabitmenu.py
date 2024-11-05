29139159c5b36a42bbf5cb481f8c1cd2180f7523
CosmicWhiskers <CosmicWhiskers@treier.net>
2024-11-05 18:46:13 +0100

    - added navigation evaluation

--- evaluation/classtest/classes/newhabitmenu.py
+++ evaluation/classtest/classes/newhabitmenu.py
@@ -32,3 +32,6 @@ class newhabitmenu(menu):
         questionary.confirm("Press any key to continue").ask()
         answer=self.show()
         self.execute(answer)
+    # TODO: remove following code, this is for reference
+    #def editHabit(self):
+     #   return    
