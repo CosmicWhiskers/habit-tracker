8d80c30f70a41b639294334e8905f9ee9c21899b
CosmicWhiskers <CosmicWhiskers@treier.net>
2024-11-18 13:02:35 +0100

    - update tests and sqlite3

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,45 @@
 import sqlite3
 import os
+import datetime
 from classes.db.habit import habit
 from classes.db.database import database
 
 class TestMain:
     con = ""
+    
+    def adapt_date_iso(val):
+        """Adapt datetime.date to ISO 8601 date."""
+        return val.isoformat()
+
+    def adapt_datetime_iso(val):
+        """Adapt datetime.datetime to timezone-naive ISO 8601 date."""
+        return val.isoformat()
+
+    def adapt_datetime_epoch(val):
+        """Adapt datetime.datetime to Unix timestamp."""
+        return int(val.timestamp())
+    def convert_date(val):
+        """Convert ISO 8601 date to datetime.date object."""
+        return datetime.date.fromisoformat(val)
+        
+    def convert_datetime(val):
+        """Convert ISO 8601 datetime to datetime.datetime object."""
+        return datetime.datetime.fromisoformat(val)
+        
+    def convert_timestamp(val):
+        """Convert Unix epoch timestamp to datetime.datetime object."""
+        return datetime.datetime.fromtimestamp(val)
+
 
     def setup_class(self):
+        sqlite3.register_adapter(datetime.date, self.adapt_date_iso)
+        sqlite3.register_adapter(datetime.datetime, self.adapt_datetime_iso)
+        sqlite3.register_adapter(datetime.datetime, self.adapt_datetime_epoch)
+
+        sqlite3.register_converter("date", self.convert_date)
+        sqlite3.register_converter("datetime", self.convert_datetime)
+        sqlite3.register_converter("timestamp", self.convert_timestamp)
+
         self.con = sqlite3.connect("testdata.db")
 
         mydb = database(self.con)
@@ -20,5 +53,16 @@ class TestMain:
         return
 
     def test_habit_create(self):
+        myhabit=habit(self.con)
+
+        myhabit.create("Test1", "TestDesc", 1)
+
+        assert myhabit.name == "Test1"
+        assert myhabit.description == "TestDesc"
+        assert myhabit.htype == 1
+
+        myhabit.insert()
+        assert myhabit.hid != 1
+
         return
 
