2dd9f644028b80b2a5f23afa208f0812d021c867
CosmicWhiskers <CosmicWhiskers@treier.net>
2024-11-18 13:02:35 +0100

    - update tests

--- classes/welcomemenu.py
+++ classes/welcomemenu.py
@@ -1,6 +1,7 @@
 from classes.menu import menu
 from classes.mainmenu import mainmenu
 import sqlite3
+import questionary
 
 class welcomemenu(menu):
     headline = "Welcome to Track My Habits"
@@ -9,6 +10,7 @@ class welcomemenu(menu):
     def show(self):
         return
     def execute(self, answer):
+        questionary.print("Creating new database if not already existing!")
         self.con.cursor().execute("""CREATE TABLE IF NOT EXISTS 
                                         habit(habit_id INTEGER PRIMARY KEY AUTOINCREMENT, 
                                               name TEXT,
@@ -21,6 +23,7 @@ class welcomemenu(menu):
                                               created_at DATETIME,
                                               lastchange_at DATETIME
                                              )""")
+        questionary.press_any_key_to_continue().ask()
         mymenu=mainmenu(self.con)
         answer=mymenu.show()
         mymenu.execute(answer)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,16 @@
-# Stub Main Unit Test file for now
-def inc(x):
-     return x + 1
+import sqlite3
+#from classes.db.habit import habit
+class TestMain:
+    con = ""
 
-def test_answer():
-     assert inc(3) == 4
+    def setup_class(self):
+        con = sqlite3.connect("testdata.db")
+        return
 
-def test_answer2():
-     assert inc(4) == 5
+    def teardown_class(self):
+        return
+
+
+    def test_habit_create(self):
+        return
+
